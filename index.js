1737389297a60982d7d65073058fa9437fc72363
Marc Valeros <marcnathanielvaleros@gmail.com>
2024-08-13 23:40:38 +0800

    made progress with hubspot and aircall

---
+++ index.js
@@ -0,0 +1,62 @@
+// const { App } = require('@slack/bolt');
+require('dotenv').config();
+const express = require('express');
+const {getAircallHubspotData, uploadFileToSlack}  = require('./util.js');
+
+//create a server 
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Aircall_Slack Webhooks! Your server is running.');
+});
+
+
+// Middleware to parse JSON request body
+app.use(express.json());
+
+
+app.post('/webhook', async (req, res) => {
+  const body = req.body;
+  console.log('Type of event:', body.event);
+
+  switch(body.event){
+    case 'call.ended':
+        // Handle call ended event
+        console.log('Call ended:', body.event);
+
+        //create an object that contains the aircall and hubspot data
+        const data = await getAircallHubspotData(body); 
+        
+        if(data){                  
+          if(data.aircall_data.duration > 120){
+            console.log("Recording Above Two Minutes");
+
+            try {
+              const res = uploadFileToSlack(data);
+            }catch(e){
+              console.log(e);
+            }
+
+          }else{
+            console.log("Recording Below Two Minutes");
+          }
+        }else{
+          console.log("Empty Data!");
+        }
+         
+        break;
+    default:
+        console.log('Unhandled event type:', body.event);    
+  }
+  
+
+  
+  res.status(200).json({ message: 'Webhook received'});
+});
+
+
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+})
---
+++ util.js
@@ -0,0 +1,115 @@
+const axios = require('axios');
+require('dotenv').config();
+
+const token = process.env.HUBSPOT_API_KEY;
+const baseURL = 'https://api.hubapi.com/';
+const endpoint = 'crm/v3/objects/companies/search';
+
+
+const uploadFileToSlack = (data) => {      //returns true if the file is already uploaded
+
+  data.aircall_data.recording
+}
+
+//search for a specific company using their phone number
+const findCompany = async (query) => {
+  if (query === 0) {
+    console.log('Query is zero. No data will be fetched.');
+    return null;
+  }
+
+  let companies;
+  const formattedPhone = formatPhoneNumber(query);
+
+    // Ensure formattedPhone is a valid value
+  if (formattedPhone === 0) {
+    console.log('Invalid phone number format.');
+    return null;
+  }
+
+  const requestBody = {
+    "filters": [
+      {
+        "propertyName": "phone",
+        "operator": "CONTAINS_TOKEN",
+        "value": `*${formattedPhone}`
+      }
+    ],
+    "sorts": [{
+        "propertyName": "createdate",
+        "direction": "DESCENDING"
+      }],
+    "properties": [
+      "name",
+      "createdate",
+      "people",
+      "email",
+      "mailing_address",
+      "dot_number",
+      "mc_number",
+      "ff_number",
+      "interstate_drivers",
+      "cdl_drivers",
+      "phone",
+    ],
+    "limit": 1,
+  };
+
+  try {
+    const response = await axios.post(`${baseURL}${endpoint}`, requestBody, {
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const companies = response.data;
+    // console.log(JSON.stringify(companies, null, 2));
+    return companies;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return null;
+  }
+
+
+}
+
+const getAircallHubspotData = async (body) =>{
+  
+  let aircall = body.data;
+  let hubspot = await findCompany(aircall.raw_digits);
+  
+  let combinedData = {
+    hubspot_id: hubspot.results[0].id,
+    hubspot_properties: hubspot.results[0].properties,
+    aircall_id: aircall.id,
+    aircall_data: aircall,
+  };
+  
+  // console.log("Combined Data: " + JSON.stringify(combinedData,null,2));
+  
+  return combinedData;
+}
+
+const formatPhoneNumber = (phoneNumber) => {
+  // Define a regular expression to match the phone number
+  const regex = /^\+1\s*(\d{3})-(\d{3})-(\d{4})$/;
+
+  // Use the regular expression to extract parts of the phone number
+  const match = phoneNumber.match(regex);
+
+  if (match) {
+      // Format the phone number as (248) 434-5508
+      return `(${match[1]}) ${match[2]}-${match[3]}`;
+  } else {
+      // If the input doesn't match the expected format, return an error or original string
+      return 0;
+  }
+}
+
+module.exports = {
+    findCompany,
+    getAircallHubspotData,
+    uploadFileToSlack
+};
+
