4176573b0fd59db761066bf5ab59d69d53606af2
Marc Valeros <marcnathanielvaleros@gmail.com>
2024-11-22 01:42:18 +0800

    improve error handling and returning status 500

--- index.js
+++ index.js
@@ -14,57 +14,38 @@ app.get('/', (req, res) => {
 // Middleware to parse JSON request body
 app.use(express.json());
 
-
 app.post('/webhook', async (req, res) => {
   const body = req.body;
-  const TWS_Aircall = process.env.TWS_AIRCALL 
-  ? JSON.parse(process.env.TWS_AIRCALL) 
-  : [];
-  console.log('Type of event:', body.event);
+  const TWS_Aircall = process.env.TWS_AIRCALL ? JSON.parse(process.env.TWS_AIRCALL) : [];
+  console.log('Received event type:', body.event);
 
-  switch(body.event){
-    case 'call.ended':
-        // Handle call ended event
-        console.log('Call ended:', body.event);
+  try {
+    switch (body.event) {
+      case 'call.ended':
+        console.log('Handling "call.ended" event...');
+        const data = await getAircallHubspotData(body);
 
-        //create an object that contains the aircall and hubspot data
-        const data = await getAircallHubspotData(body); 
-        
-        console.log(JSON.stringify(data,null,1));
-        
-        if(data && data.aircall_data.duration > process.env.DURATION){
-          if(TWS_Aircall.includes(data.aircall_data.user.id)){
-            console.log("Data will be sent to another slack channel");
-            console.log(`The aircall user name is ${data.aircall_data.user.name}`);
+        if (data && data.aircall_data.duration > process.env.DURATION) {
+          const isTwsUser = TWS_Aircall.includes(data.aircall_data.user.id);
+          const channelId = isTwsUser ? process.env.DCS_TRAINING_CHANNEL_ID : process.env.SLACK_CHANNEL_ID;
 
-            try {
-              uploadFileToSlack(data, process.env.DCS_TRAINING_CHANNEL_ID);
-            }catch(e){
-              console.log(e);
-            }
-
-          }else{
-              try {
-                uploadFileToSlack(data, process.env.SLACK_CHANNEL_ID);
-              }catch(e){
-                console.log(e);
-              }
-          }
-
-        }else{
-          console.log("Empty Data!");
+          console.log(`Uploading data to Slack channel. User: ${data.aircall_data.user.name}, Channel: ${channelId}`);
+          await handleSlackUpload(data, channelId);
+        } else {
+          console.log('Data is empty or does not meet duration criteria.');
         }
-         
         break;
-    default:
-        console.log('Unhandled event type:', body.event);    
+
+      default:
+        console.log('Unhandled event type:', body.event);
+    }
+
+    res.status(200).json({ message: 'Webhook received' });
+  } catch (error) {
+    console.error('Error processing webhook:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
-  
-
-  
-  res.status(200).json({ message: 'Webhook received'});
 });
-
 
 
 const port = process.env.PORT || 8080;
