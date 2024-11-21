13e554d9e261707f4979bb6977a589871057110f
Marc Valeros <marcnathanielvaleros@gmail.com>
2024-11-22 01:57:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const {getAircallHubspotData}  = require('./util.js');
+const {uploadFileToSlack}  = require('./slack.js');
 
 //create a server 
 const app = express();
@@ -15,7 +16,6 @@ app.use(express.json());
 
 app.post('/webhook', async (req, res) => {
   const body = req.body;
-  // const TWS_Aircall = process.env.TWS_AIRCALL ? JSON.parse(process.env.TWS_AIRCALL) : [];
   const TWS_Aircall = [1452638, 1452635, 1452641, 1452620];
   console.log('Received event type:', body.event);
 
@@ -30,7 +30,7 @@ app.post('/webhook', async (req, res) => {
           const channelId = isTwsUser ? process.env.DCS_TRAINING_CHANNEL_ID : process.env.SLACK_CHANNEL_ID;
 
           console.log(`Uploading data to Slack channel. User: ${data.aircall_data.user.name}, Channel: ${channelId}`);
-          await handleSlackUpload(data, channelId);
+          await uploadFileToSlack(data, channelId);
         } else {
           console.log('Data is empty or does not meet duration criteria.');
         }
